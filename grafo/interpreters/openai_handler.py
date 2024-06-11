8e18a6b959e5f6f23b9bb61a65d321b0e4c5ba96
paulomtts <paulomtts@outlook.com>
2024-06-11 19:28:39 -0300

    feat: fixes

--- grafo/interpreters/openai_handler.py
+++ grafo/interpreters/openai_handler.py
@@ -2,9 +2,11 @@ import os
 from typing import Literal, Optional
 
 import instructor
-from openai import OpenAI
+from langsmith import traceable
+from langsmith.wrappers import wrap_openai
+from openai import AsyncOpenAI, OpenAI
 
-from .base import BaseLLM
+from grafo.interpreters.base import BaseLLM
 
 
 class OpenAIHandler(BaseLLM):
@@ -17,18 +19,73 @@ class OpenAIHandler(BaseLLM):
     ):
         super().__init__()
 
-        self.client = instructor.from_openai(client=OpenAI())
+        # self.client = instructor.from_openai(client=OpenAI())
         self.model = os.getenv("OPENAI_MODEL") or model
         self.temperature = temperature
 
-    def send(self, messages: list, response_model, model: Optional[str] = "gpt-4o"):
+        self._langsmith = True
+        if os.getenv("LANGCHAIN_API_KEY") is None:
+            self._langsmith = False
+
+    @property
+    def langsmith(self):
+        return self._langsmith
+
+    def send(
+        self,
+        messages: list,
+        response_model,
+        model: Optional[str] = "gpt-4o",
+        max_retries: int = 3,
+    ):
         """
         Send data to the model.
         """
-        return self.client.chat.completions.create(
-            model=model, messages=messages, response_model=response_model
+        if self.langsmith:
+            client = wrap_openai(OpenAI())
+        else:
+            client = OpenAI()
+
+        client = instructor.from_openai(
+            client=client, mode=instructor.Mode.TOOLS
+        )  # NOTE: keep TOOLS instead of PARALLEL_TOOLS for now
+
+        return client.chat.completions.create(
+            model=model,
+            messages=messages,
+            response_model=response_model,
+            max_retries=max_retries,
         )
 
+    @traceable(name="send-async")
+    async def asend(
+        self,
+        messages: list,
+        response_model,
+        model: Optional[str] = "gpt-4o",
+        max_retries: int = 3,
+    ):
+        """
+        Sends data to the model asyncronously.
+        """
+        if self.langsmith:
+            client = wrap_openai(AsyncOpenAI())
+        else:
+            client = AsyncOpenAI()
+
+        client = instructor.from_openai(
+            client=client, mode=instructor.Mode.TOOLS
+        )  # NOTE: keep TOOLS instead of PARALLEL_TOOLS for now
+
+        return await client.chat.completions.create(
+            model=model,
+            messages=messages,
+            response_model=response_model,
+            max_retries=max_retries,
+        )
+
+    ################################################################################
+    # Beta
     def create__assistant(
         self,
         client: OpenAI,
@@ -80,3 +137,5 @@ class OpenAIHandler(BaseLLM):
             "type": "image_file",
             "image_file": {"file_id": new_file.id},
         }
+
+    #############################################################################
--- setup.py
+++ setup.py
@@ -1,9 +1,18 @@
 from setuptools import setup, find_packages
 
+with open("README.md", encoding="utf-8") as f:
+    long_description = f.read()
+
 setup(
     name="grafo",
-    version="0.1.0",
+    version="0.1.1",
+    description="A library for building runnable asynchronous trees",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Paulo Mattos",
+    author_email="paulomtts@outlook.com",
+    url="https://github.com/paulomtts/grafo",
     packages=find_packages(include=["grafo", "grafo.*"]),
-    install_requires=["openai"],
-    entry_points={},
+    install_requires=["openai", "instructor", "langsmith"],
+    python_requires=">=3.6",
 )

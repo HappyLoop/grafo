9960a9c1c428c8936d205d7d76ba787830d4ad00
paulomtts <paulomtts@outlook.com>
2024-06-11 20:28:47 -0300

    fix: workflow  readme.me error

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
-with open("./README.md", encoding="utf-8") as f:
+with open("./readme.md", encoding="utf-8") as f:
     long_description = f.read()
 
 setup(
     name="grafo",
-    version="0.1.1",
+    version="0.1.2",
     description="A library for building runnable asynchronous trees",
     long_description=long_description,
     long_description_content_type="text/markdown",
